79dc1e1b4216ed0192b98e290eb7caae4922aeed
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-12 10:54:22 +0200

    Items

--- app.js
+++ app.js
@@ -52,7 +52,14 @@ const randomBtn = document.querySelector('.random-btn');
 let currentItem = 0;
 
 // load iniitial item
-window.addEventListener("DOMContentLoaded", () => {
-  const item = reviews[currentItem];
+window.addEventListener("DOMContentLoaded", () => { });
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
-});
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.info;
+}
